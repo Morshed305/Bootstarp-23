b6217adae42ccdee21996639d6ccc253342500fa
Moreshed <morshedalam9691616@gmail.com>
2024-04-07 15:13:32 +0600

    Text-Bootstrap

---
+++ Bootstarp-Final_Exam/script.js
@@ -0,0 +1,116 @@
+// function Sum(a , b){
+//     return a + b;
+// }
+// console.log(Sum(5 , 10));
+
+
+
+
+// const Array = [12, 5, 27, 8, 16, 80,2];
+// function big_number (a) { 
+//     let x = Array[0];
+//     for (let i = 1; i < Array.length; i++) {
+//       if (Array[i] < x) {
+//         x = Array[i];
+//       }
+//     }
+//     return x;
+//   }
+//   console.log(big_number (Array));
+
+
+
+
+// let str = "Hello, World!a";
+//   function count(a) {
+//     var x = str.length;
+//     return x;
+//   }
+//   console.log( count(str));
+
+ 
+
+// var int =11;
+//   function count(a) {
+//     if(int % 2 == 0){
+//         return 'Even';
+//     }else{
+//         return 'Odd'
+//   }}
+//   console.log(count(int));
+
+
+
+
+
+// const Array =[200,10,60,500,34,56,78,55]
+// function AllValues(b) {
+//   for (var i = 0; i < Array.length; i++) {
+//     console.log(Array[i]);
+//   }
+// }
+// AllValues(Array)
+
+
+
+
+
+
+
+
+
+// const Array =[200,10,60,700,34,56,78,55]
+// let x=0;
+// function mainFunction (a){
+
+// for(let i=0; i< Array.length ; i++){
+// if(x<Array[i]){
+//   x=Array[i];  
+// }}
+// return x;
+// }
+// console.log(mainFunction(Array));
+
+
+
+
+
+
+// function oddOrEvenString(Array) {
+//     var length = Array.length;
+//     if (length % 2 === 0) {
+//       return 'Even';
+//     } else {
+//       return 'Odd';
+//     }
+//   }
+//   var myString = "Hello, World!";
+//   var result = oddOrEvenString(myString);
+  
+//   console.log('Length of the string is:', result);
+
+
+
+
+// function arrsum(Array) {
+//     var sum = 0;
+//     for (var i = 0; i < Array.length; i++) {
+//       sum += Array.charCodeAt(i);
+//     }
+//     return sum;
+//   }
+//   var myString = "Hello, World!";
+//   var result = arrsum(myString);
+  
+//   console.log('Sum of ASCII values:', result);'
+
+
+
+
+
+
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(20,300));
